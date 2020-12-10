fb3a731a6ad89eb5bec69119b966aa69774819d2
inabajunmr <inabajun.for.regi@gmail.com>
2020-12-10 23:43:21 +0900

    unuse private field for firefox

--- hannya64.js
+++ hannya64.js
@@ -1,6 +1,6 @@
 class Hannya64 {
-    #base64chars
-    #padding
+    base64chars
+    padding
     constructor(chars, padding) {
         if (chars.length != 64) {
             throw new Error("Chars 64 length text.")
@@ -8,39 +8,40 @@ class Hannya64 {
         if (padding.length != 1) {
             throw new Error("Padding requires single letter.")
         }
-        this.#base64chars = chars
-        this.#padding = padding
+        this.base64chars = chars
+        this.padding = padding
     }
 
     encode(bytes) {
-        const padding = this.#paddingCount(bytes)
+        const padding = this.paddingCount(bytes)
         const b = new Uint8Array(bytes.length + padding);
         b.set(bytes)
         let result = ""
         for (let i = 0; i < b.length; i += 3) {
-            const a24 = b[i] << 16 | b[i + 1] << 8 | b[i + 2] // 3byte to 24bit int
-            const a1 = a24 >> 18 & 0b111111; // 1st 6bit
-            const a2 = a24 >> 12 & 0b111111; // 2nd 6bit
-            const a3 = a24 >> 6 & 0b111111; // 3rd 6bit
-            const a4 = a24 & 0b111111; // 4th 6bit
+            // 3byte to 24bit int(111111112222222233333333)
+            const a24 = b[i] << 16 | b[i + 1] << 8 | b[i + 2]
+            const a1 = a24 >> 18 & 0b111111; // 1st 6bit 111111
+            const a2 = a24 >> 12 & 0b111111; // 2nd 6bit 112222
+            const a3 = a24 >> 6 & 0b111111; // 3rd 6bit 222233
+            const a4 = a24 & 0b111111; // 4th 6bit 333333
 
             // assemble encoded string
-            result += this.#base64chars.charAt(a1)
-            result += this.#base64chars.charAt(a2)
+            result += this.base64chars.charAt(a1)
+            result += this.base64chars.charAt(a2)
             if (i + 2 == b.length - 1) {
                 if (padding <= 1) {
-                    result += this.#base64chars.charAt(a3);
+                    result += this.base64chars.charAt(a3);
                 } else {
-                    result += this.#padding;
+                    result += this.padding;
                 }
                 if (padding == 0) {
-                    result += this.#base64chars.charAt(a4);
+                    result += this.base64chars.charAt(a4);
                 } else {
-                    result += this.#padding;
+                    result += this.padding;
                 }
             } else {
-                result += this.#base64chars.charAt(a3);
-                result += this.#base64chars.charAt(a4);
+                result += this.base64chars.charAt(a3);
+                result += this.base64chars.charAt(a4);
             }
         }
         return result
@@ -50,18 +51,18 @@ class Hannya64 {
         let result = []
         // 4chars(6bit) to 24byte(111111222222333333444444)
         for (let i = 0; i < encoded.length; i += 4) {
-            const i1 = this.#base64chars.indexOf(encoded.charAt(i))
-            const i2 = this.#base64chars.indexOf(encoded.charAt(i + 1))
+            const i1 = this.base64chars.indexOf(encoded.charAt(i))
+            const i2 = this.base64chars.indexOf(encoded.charAt(i + 1))
             const b1 = i1 << 2 | i2 >> 4; // 11111122
             result.push(b1)
             let i3 = 0;
-            if (encoded.charAt(i + 2) != this.#padding) {
-                i3 = this.#base64chars.indexOf(encoded.charAt(i + 2));
+            if (encoded.charAt(i + 2) != this.padding) {
+                i3 = this.base64chars.indexOf(encoded.charAt(i + 2));
                 const b2 = i2 << 4 & 0b11110000 | i3 >> 2; // 22223333
                 result.push(b2)
             }
-            if (encoded.charAt(i + 3) != this.#padding) {
-                const i4 = this.#base64chars.indexOf(encoded.charAt(i + 3));
+            if (encoded.charAt(i + 3) != this.padding) {
+                const i4 = this.base64chars.indexOf(encoded.charAt(i + 3));
                 const b3 = i3 << 6 & 0b11000000 | i4; // 33444444
                 result.push(b3)
             }
@@ -81,7 +82,7 @@ class Hannya64 {
         return decoder.decode(decoded)
     }
 
-    #paddingCount(bytes) {
+    paddingCount(bytes) {
         if (bytes.length % 3 == 1) {
             return 2;
         } else if (bytes.length % 3 == 2) {
